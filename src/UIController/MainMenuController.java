2f48559a44592e037a56150e99f8855c0f246552
Shyket <munimulislam@iut-dhaka.edu>
2020-12-21 11:53:18 +0600

    linked UI with controller

---
+++ src/UIController/MainMenuController.java
@@ -0,0 +1,53 @@
+package UIController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainMenuController implements Initializable {
+
+    @FXML private AnchorPane rootPane;
+    @FXML private Button productButton;
+    @FXML private Button categoryButton;
+    @FXML private Button transactionButton;
+    @FXML private Button dueButton;
+    @FXML private Button profitButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void productButtonEventHandle(ActionEvent event) {
+
+        try {
+            FileInputStream f = new FileInputStream(
+                    new File(System
+                            .getProperty("user.dir")
+                            .concat("\\src\\UI\\" +
+                                    "Product.fxml")));
+
+            FXMLLoader loader = new FXMLLoader();
+            AnchorPane newPane = loader.load(f);
+
+            if(rootPane == null){
+                System.out.println("null");
+            }
+
+            rootPane.getChildren().setAll(newPane);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
