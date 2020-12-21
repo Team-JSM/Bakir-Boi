334db8e5978b621206e3911031c570a33e7618e3
Shyket <munimulislam@iut-dhaka.edu>
2020-12-21 14:42:52 +0600

    productlist page design and controller for that is added

--- src/UIController/InventoryController.java
+++ src/UIController/InventoryController.java
@@ -1,9 +1,9 @@
 package UIController;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.File;
@@ -11,32 +11,42 @@ import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-public class MainMenuController implements Initializable {
+public class InventoryController implements Initializable {
 
     @FXML private AnchorPane rootPane;
-    @FXML private Button productButton;
-    @FXML private Button categoryButton;
-    @FXML private Button transactionButton;
-    @FXML private Button dueButton;
-    @FXML private Button profitButton;
-
+    @FXML private AnchorPane rightPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
-
+        productButtonEventHandle();
     }
 
-    public void inventoryButtonEventHandle(ActionEvent event) {
+    public void productButtonEventHandle() {
+        try {
+            FileInputStream f = new FileInputStream(
+                    new File(System
+                            .getProperty("user.dir")
+                            .concat("\\src\\UI\\" +
+                                    "ProductList.fxml")));
+
+            FXMLLoader loader = new FXMLLoader();
+            AnchorPane newPane = loader.load(f);
+
+            rightPane.getChildren().setAll(newPane);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void homeButtonEventHandle(ActionEvent event) {
 
         try {
             FileInputStream f = new FileInputStream(
                     new File(System
                             .getProperty("user.dir")
                             .concat("\\src\\UI\\" +
-                                    "Inventory.fxml")));
+                                    "MainMenu.fxml")));
 
             FXMLLoader loader = new FXMLLoader();
             AnchorPane newPane = loader.load(f);
@@ -48,4 +58,6 @@ public class MainMenuController implements Initializable {
         }
 
     }
+
+
 }
